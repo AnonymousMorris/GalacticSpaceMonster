8908607394e4e6f896e3e60ec39482bc8a57c328
AnonymousMorris <morrislin13@gmail.com>
2023-11-15 19:11:04 -0500

    fixed bug, the player can move now. New bug: The player gets stuck after running into the world border

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ function dist(x1, y1, x2, y2){
     }
     return Math.sqrt(x1*x1 + y1*y1);
 }
-
+const mousePos = {
+    x : 0,
+    y : 0
+}
 
 class ball{
     canvas;
@@ -48,11 +51,31 @@ class Player {
         this.y = 0;
     }
     //todo change the method so that it can be called in the game update function instead of by even listener so that it is consistent.
-    update(dx, dy){
+    update(){
+        this.updatePos();
+    }
+    updatePos() {
+        const relativeX = mousePos.x - centerX;
+        const relativeY = mousePos.y - centerY;
+        let distance = 0;
+        if(dist){ //check to make sure the function exists before using
+            distance = dist(relativeX, relativeY);
+        }
+        // normalize the dx and dy
+        let dx = relativeX / distance;
+        let dy = relativeY / distance;
+        // create a dampening effect when mouse is close to player
+        if(distance < 10){
+            dx *= distance / 15;
+            dy *= distance / 15;
+        }
         if(dist(this.x, this.y, 0, 0) < WORLD_RADIUS){
             this.x += dx;
             this.y += dy;
         }
+    }
+    updateDirection(){
+        //todo point sprite to the mouse
     }
     render(){
         this.context.beginPath();
@@ -85,7 +108,7 @@ class world{
         this.context.beginPath();
         this.context.moveTo(this.x, this.y);
         this.context.arc(-this.player.x + centerX, -this.player.y + centerY, this.radius, 0, 2* Math.PI);
-        // this.context.fill();
+        this.context.fill();
     }
 }
 class Game{
@@ -99,7 +122,9 @@ class Game{
         this.world = new world(canvas, ctx, WORLD_RADIUS, this.player);
     }
     update(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.world.update();
+        this.player.update();
 
     }
     render(){
@@ -116,18 +141,8 @@ function animate(){
     requestAnimationFrame(animate)
 }
 
-function createMouseMoveHandler(player) {
-    return (e) =>{
-        const relativeX = e.clientX - canvas.offsetLeft;
-        const relativeY = e.clientY - canvas.offsetTop;
-        let distance = 0;
-        if(dist){
-            distance = dist(relativeX, relativeY);
-        }
-        const dx = relativeX / distance;
-        const dy = relativeY / distance;
-        player.update(dx, dy)
-    }
-}
-const mouseMoveHandler = createMouseMoveHandler(game.player)
-canvas.addEventListener("mousemove", mouseMoveHandler, false);
+
+canvas.addEventListener("mousemove", e => {
+    mousePos.x = e.clientX - canvas.offsetLeft;
+    mousePos.y = e.clientY - canvas.offsetTop;
+}, false);
