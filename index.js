2c40b82377d5c4bd0f3a0c20ba30bbf8d9752ab1
AnonymousMorris <morrislin13@gmail.com>
2023-11-19 14:46:00 -0500

    removed some changes I made earlier for debugging purposes

--- index.js
+++ index.js
@@ -34,29 +34,19 @@ class planetPool{
         this.blockHeight = Math.round(game.canvas.height / 2) + MAX_PLANET_RADIUS;
         this.numX = Math.floor(WORLD_RADIUS * 2 / this.blockWidth ) + 1;
         this.numY = Math.floor(WORLD_RADIUS * 2 / this.blockHeight) + 1;
-        console.log("block width:" + this.blockWidth);
-        console.log("numX: " + this.numX);
-        console.log("block height:" + this.blockHeight);
-        console.log("numY: " + this.numY);
         for(let i = 0; i < this.numX; i++){
             this.planetPool[i] = []
             const offsetX = i * this.blockWidth - WORLD_RADIUS;
             for(let j = 0; j < this.numY; j++){
                 this.planetPool[i][j] = [];
                 const offsetY = -j * this.blockHeight + WORLD_RADIUS;
-                // const n = Math.round(Math.random() * 5);
-                const n = 50;
+                const n = Math.round(Math.random() * 5);
                 for(let k = 0; k < n; k++){
                     const planetPosX = Math.random() * this.blockWidth + offsetX;
-                    // const planetPosX = offsetX;
-                    const planetPosY = offsetY;
-                    // const planetPosY = -Math.random() * this.blockHeight + offsetY;
+                    const planetPosY = -Math.random() * this.blockHeight + offsetY;
                     const planetRadius = Math.random() * MAX_PLANET_RADIUS / 2 + MAX_PLANET_RADIUS / 2;
-                    //logging some stuff
                     this.planetPool[i][j].push(new planet(this.game, planetPosX, planetPosY, planetRadius))
                 }
-                console.log(offsetX);
-                console.log(offsetY);
             }
         }
     }
